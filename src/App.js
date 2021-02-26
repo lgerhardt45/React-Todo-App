234f405005191f4f2700a94ff623ffadc0c90abc
Lukas Gerhardt <lukas.gerhardt@tum.de>
2021-02-26 21:16:22 +0100

    add first two components and their html code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+
+// Importing Component
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <h1>Lukas' To-Do List</h1>
+      <Form />
+      <TodoList />
     </div>
   );
 }
